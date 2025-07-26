eddc208a7a547abb705fc254bd2a5ba7e40c1673
Juan Rodríguez <juanitorodrialonso@gmail.com>
2025-07-26 18:17:51 +0200

    Initial files for the project's structure

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello, World!");
+    return $response;
+});
+
+$app->run();
